8a75d4b6f468ef65850defe421a4788742b2aac9
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-08-02 11:59:02 +0530

    Added ASCII artwork

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,3 +1,13 @@
+print('''
+  _____  _                           _      _____      _            _       _
+ |  __ \(_)                         | |    / ____|    | |          | |     | |
+ | |  | |_ ___  ___ ___  _   _ _ __ | |_  | |     __ _| | ___ _   _| | __ _| |_ ___  _ __
+ | |  | | / __|/ __/ _ \| | | | '_ \| __| | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__|
+ | |__| | \__ \ (_| (_) | |_| | | | | |_  | |___| (_| | | (__| |_| | | (_| | || (_) | |
+ |_____/|_|___/\___\___/ \__,_|_| |_|\__|  \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
+                                                                                                                                                                                
+''')
+
 total_bill = int(input("What is your total bill? "))
 discount = int(input("How much discount are you getting? "))
 additional_discount = input("Are you getting any additional discount/cashback? Y or N ")
@@ -13,7 +23,7 @@ if additional_discount == "Y":
     total_discount_after_additional_discount = add_discount * bill_before_additional_discount / 100
     total_bill_after_all_discount = bill_before_additional_discount - total_discount_after_additional_discount
     print(f"Your absolute total is {total_bill_after_all_discount}")
-    
+
 
 else:
     bill_after_discount = discount * total_bill / 100
